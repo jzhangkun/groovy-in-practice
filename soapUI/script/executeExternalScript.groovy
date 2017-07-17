b02a7e2bf9f96d83f8172008ef053d204bf135ca
jzhangkun <jzhangkun1986@gmail.com>
2017-07-18 00:26:34 +0800

    Load external groovy class into soapUI

--- soapUI/script/executeExternalScript.groovy
+++ soapUI/script/executeExternalScript.groovy
@@ -1,28 +1,25 @@
 /**
  * Created by jzhangkun on 09/07/2017.
  */
-
 import groovy.lang.Binding
 import groovy.util.GroovyScriptEngine
 
-def groovyUtils = new com.eviware.soapui.support.GroovyUtils( context )
-
-log.info(groovyUtils)
-
-//log.info(groovyUtils.getProjectPath)
+def groovyUtils = new com.eviware.soapui.support.GroovyUtils( context );
 
 // define location of script file relative to project file.
-String projectPath = groovyUtils.getProjectPath() + "/groovy/";
+String projectPath = groovyUtils.getProjectPath()
 log.info(orojectPath);
+String newProjectPath = projectPath + "../src/groovy/com/kun"
 
 // Create Groovy Script Engine to run Script.
-GroovyScriptEngine gse = new GroovyScriptEngine(projectPath)
+GroovyScriptEngine gse = new GroovyScriptEngine(newProjectPath)
 
 // Load the Groovy Script file
-externalScript = gse.loadScriptByName("Utility.groovy")
+externalScript = gse.loadScriptByName("Hello.groovy")
 
 // Create a runtime instance of script
 instance = externalScript.newInstance()
+log.info(instance)
 
 // Sanity check
 assert instance!= null
---
+++ soapUI/src/groovy/com/kun/Hello.groovy
@@ -0,0 +1,11 @@
+package groovy.com.kun
+
+/**
+ * Created by jzhangkun on 18/07/2017.
+ */
+class Hello {
+    def sayHello(name)
+    {
+        return "Hello " + name + "!";
+    }
+}
